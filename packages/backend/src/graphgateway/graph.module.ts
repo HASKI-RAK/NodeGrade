620dc127e914c4f4a7020755d8aebb285d406342
theUpsider <davidvfischer@gmail.com>
2025-04-03 14:00:00 +0200

    feat: update launch configuration for debugging and modify backend script for development; add PrismaService to GraphModule providers

--- packages/backend/src/graphgateway/graph.module.ts
+++ packages/backend/src/graphgateway/graph.module.ts
@@ -2,8 +2,9 @@ import { Module } from '@nestjs/common';
 import { GraphGateway } from './graph.gateway';
 import { GraphService } from '../graph/graph.service';
 import { GraphHandlerService } from './graph-handler.service';
+import { PrismaService } from 'src/prisma.service';
 
 @Module({
-  providers: [GraphGateway, GraphService, GraphHandlerService],
+  providers: [GraphGateway, GraphService, GraphHandlerService, PrismaService],
 })
 export class GraphModule {}
