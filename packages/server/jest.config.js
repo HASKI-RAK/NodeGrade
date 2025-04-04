ab9118f090bc00e04885bb0d7824976a8c44ac71
theUpsider <davidvfischer@gmail.com>
2025-04-04 15:37:00 +0200

    removed old servercode

--- packages/server/jest.config.js
+++
@@ -1,7 +0,0 @@
-/* eslint-disable immutable/no-mutation */
-module.exports = {
-  preset: 'ts-jest',
-  testEnvironment: 'node',
-  collectCoverage: true,
-  setupFilesAfterEnv: ['<rootDir>/singleton.ts']
-}
--- packages/server/types/wsaa/index.d.ts
+++
@@ -1,5 +0,0 @@
-export * from 'ws/index.mjs'
-import { WebSocket as WS } from 'ws/index.mjs'
-export type WebSocket = WS & {
-  sendEvent<K extends keyof ServerEventPayload>(event: ServerEvent<K>): void
-}
